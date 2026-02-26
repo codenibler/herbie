e1d47b4b2f45ae366c40200f152369586e0b826d
codenibler <angel.barrio.requejo@gmail.com>
2026-02-26 21:52:31 +0100

    Generic marvin catchphrases

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # Entry point for the application
+from piper_tts import read_out_response, read_out_response_from_file
 from ollama_model import ollama_query, warm_up_ollama_model
 from user_listening_loop import listen_for_user_input
 from wakeword_loop import initialize_wakeword_loop
 from parse_user_input import parse_user_input
-from piper_tts import read_out_response
 from log_setup import setup_logging
 from dotenv import load_dotenv
 from gpiozero import Buzzer
+from pathlib import Path
 from time import sleep
 
 import logging
+import random
 import os
 
 BUZZER_PIN = 2
+GENERIC_MARVIN_RESPONSES_DIR = "generic_marvin_responses"
 
 # Testing function for wakeword
 """ NEED TO IMPORT OLLAMA AND START TESTING SPEEDS AND SENDING MESSAGES"""
@@ -24,31 +27,37 @@ def activate_buzzer():
         buzzer.off()
         sleep(0.1)
 
-
 def main():
+    # Initial setup
     load_dotenv(override=True) # Override environemnt vars with those in .env
     setup_logging() 
-    """ TO DO: ADD BACKGROUND NOISE LEVEL CALIBRATOR ASYNCED WHILE WE WARM UP MODEL. """
+    """ TO DO: ADD BACKGROUND NOISE LEVEL CALIBRATOR  WHILE WE WARM UP MODEL. """
+    """ - Probably not enough to do once on startup, should be more frequent """
     warm_up_ollama_model()  # Warm up with system prompt. 
 
-    wakeword_detected = initialize_wakeword_loop() # Returns when heard
-    activate_buzzer()  # Buzz to indicate wake word was detected
+    while True:
+        wakeword_detected = initialize_wakeword_loop() # Returns when heard
+        marv_responses = os.listdir(GENERIC_MARVIN_RESPONSES_DIR)
+        logging.info(f"Selecting random Marv response from {GENERIC_MARVIN_RESPONSES_DIR}...")
+        logging.info("Available Marv responses: " + ", ".join(marv_responses))
 
-    """ TO DO : ADD GENERIC HERBIE RESPONSE, LIKE 'HEY BOSS!'"""
+        random_marv_response = random.choice(marv_responses)
+        read_out_response_from_file(Path(f"{GENERIC_MARVIN_RESPONSES_DIR}/{random_marv_response}"))
 
-    if wakeword_detected:
-        """ TO DO: SET UP LED ANIMATIONS AND SOUND FOR HERBIE ACTIVATION """
-        wav_bytes = listen_for_user_input()
-    
-    """ TO DO: ADD ERROR LIMIT UPPER BOUND, AND SET THIS UP AS MORE ROBUST SUPERLOOP """
-    user_text = parse_user_input(wav_bytes)
-    if user_text is None:
-        logging.error("Failed to parse user input. Retrying...")
-        user_text =listen_for_user_input()  # Optionally, you could add a retry limit here.
-    
-    ollama_response = ollama_query(user_text)
-    read_out_response(ollama_response)  
-    activate_buzzer()
+        if wakeword_detected:
+            """ TO DO: SET UP LED ANIMATIONS AND SOUND FOR HERBIE ACTIVATION """
+            wav_bytes = listen_for_user_input()
+        
+        """ TO DO: ADD ERROR LIMIT UPPER BOUND, AND SET THIS UP AS MORE ROBUST SUPERLOOP """
+        user_text = parse_user_input(wav_bytes)
+        if user_text is None:
+            logging.error("Failed to parse user input. Retrying...")
+            user_text =listen_for_user_input()  # Optionally, you could add a retry limit here.
+        
+        ollama_response = ollama_query(user_text)
+        read_out_response(ollama_response)  
+        activate_buzzer()
+
 
 if __name__ == "__main__":
     main()
--- piper_tts.py
+++ piper_tts.py
@@ -1,6 +1,10 @@
-import logging
 import subprocess
+import logging
 import wave
+import sys
+import os
+
+from pathlib import Path
 from piper import PiperVoice, SynthesisConfig
 
 syn_config = SynthesisConfig(
@@ -11,12 +15,27 @@ syn_config = SynthesisConfig(
     normalize_audio=False,
 )
 
-voice = PiperVoice.load("piper_voice_model/en_US-kusal-medium.onnx")  # load once
+voice_model_path = os.getenv("PIPER_VOICE_MODEL_PATH", "piper_voice_model/en_US-lessac-medium.onnx")
+voice = PiperVoice.load(voice_model_path) 
 
 def read_out_response(text: str):
-    out_wav = "test.wav"
-    with wave.open(out_wav, "wb") as wav_file:
+
+    RESPONSE_AUDIO_DIR = os.getenv("RESPONSE_AUDIO_DIR", "response_audio")
+    out_wav = Path(RESPONSE_AUDIO_DIR) / f"{text}.wav"
+    out_wav.parent.mkdir(parents=True, exist_ok=True)  
+
+    logging.debug(f"Synthesizing response to {out_wav}...")
+
+    with wave.open(str(out_wav), "wb") as wav_file:
         voice.synthesize_wav(text, wav_file, syn_config=syn_config)
 
     logging.info("Playing audio via aplay...")
-    subprocess.run(["aplay", "-q", out_wav], check=False)
+    subprocess.run(["aplay", "-q", str(out_wav)], check=False)
+
+
+def read_out_response_from_file(file_path: str):
+    subprocess.run(["aplay", "-q", file_path], check=False)
+
+
+if __name__ == "__main__":
+    read_out_response(sys.argv[1])
